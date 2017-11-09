c0d068465346cc52a248b153fb1f62026d32b267
Esten Leonardsen <estenleonardsen@gmail.com>
2017-11-09 21:34:40 +0100

    Fixed append lr

--- tfpreprocessing/append_flip_left_right.py
+++ tfpreprocessing/append_flip_left_right.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 def append_flip_left_right(X, y):
     flipped = tf.map_fn(lambda img: tf.image.flip_left_right(img), X)
-    X = tf.concat([X, flipped])
-    y = tf.concat([y, y])
+    X = tf.concat([X, flipped], axis=0)
+    y = tf.concat([y, y], axis=0)
 
     return X, y
