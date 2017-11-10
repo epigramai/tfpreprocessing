6bdd9ef763a873f1153f5d16616d4d07a02e417b
Esten Leonardsen <estenleonardsen@gmail.com>
2017-11-10 13:57:16 +0100

    Added random size to random crops

--- tfpreprocessing/random_crops_and_resize.py
+++ tfpreprocessing/random_crops_and_resize.py
@@ -4,7 +4,10 @@ def random_crops_and_resize(tensor, *, min_size, name):
     height, width, channels = [int(x) for x in tensor.shape[1:]]
     min_height, min_width = min_size
 
-    cropped = tf.map_fn(lambda x: tf.random_crop(x, [min_height, min_width, channels]), tensor, name=name + '/cropped')
+    crop_height = tf.random_uniform([], minval=min_height, maxval=height, dtype=tf.int64)
+    crop_width = tf.random_uniform([], minval=min_width, maxval=width, dtype=tf.int64)
+
+    cropped = tf.map_fn(lambda x: tf.random_crop(x, [crop_height, crop_width, channels]), tensor, name=name + '/cropped')
     resized = tf.image.resize_images(cropped, [height, width])
 
     return resized
