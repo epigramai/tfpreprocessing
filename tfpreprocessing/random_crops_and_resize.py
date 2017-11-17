45bc003bb168057c4bae85fe66a462b928d8d25e
Esten Leonardsen <estenleonardsen@gmail.com>
2017-11-17 18:04:38 +0100

    Fixed crops

--- tfpreprocessing/random_crops_and_resize.py
+++ tfpreprocessing/random_crops_and_resize.py
@@ -1,13 +1,18 @@
 import tensorflow as tf
 
-def random_crops_and_resize(tensor, *, min_size, name):
-    height, width, channels = [int(x) for x in tensor.shape[1:]]
+
+def random_crop_and_resize(tensor, *, min_size):
+    height, width, channels = [int(x) for x in tensor.shape]
     min_height, min_width = min_size
 
-    crop_height = tf.random_uniform([], minval=min_height, maxval=height, dtype=tf.int64)
-    crop_width = tf.random_uniform([], minval=min_width, maxval=width, dtype=tf.int64)
+    crop_height = tf.random_uniform([], minval=min_height, maxval=max_height, dtype=tf.int64)
+    crop_width = tf.random_uniform([], minval=min_width, maxval=max_width, dtype=tf.int64)
 
-    cropped = tf.map_fn(lambda x: tf.random_crop(x, [crop_height, crop_width, channels]), tensor, name=name + '/cropped')
+    cropped = tf.random_crop(x, [crop_height, crop_width, channels])
     resized = tf.image.resize_images(cropped, [height, width])
 
     return resized
+
+
+def random_crops_and_resize(tensor, *, min_size, name):
+    return tf.map_fn(lambda img: random_crop_and_resize(img, min_size), tensor)
